214f422ee198fa8b63937e67e1af8ce5e31a31ad
Вікторія Кубинець <154357828+viktoriia20046@users.noreply.github.com>
2024-10-31 18:23:11 +0100

    Виправлено імпорт модуля: оновлено шлях до 'quotes_scraper.spiders.quotes' у main.py

--- quote_scrapy_project/quotes_scraper/main.py
+++ quote_scrapy_project/quotes_scraper/main.py
@@ -1,6 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from quote_scrapy_project.spiders.quotes import QuotesSpider
+from quotes_scraper.spiders.quotes import QuotesSpider
 
 process = CrawlerProcess(settings=get_project_settings())
 
